5722b69b778e2316330fa00d24b5e8955008d3a7
Tueur_de_l€ak <vdsisaac410@gmail.com>
2025-10-29 02:47:57 +0100

    Create EmptyState.jsx

---
+++ src/components/common/EmptyState.jsx
@@ -0,0 +1,7 @@
+const EmptyState = ({ message }) => (
+  <div className="flex justify-center items-center h-full min-h-[50vh] text-center">
+    <p className="text-xl text-gray-500 dark:text-gray-400">{message}</p>
+  </div>
+);
+
+export default EmptyState;
