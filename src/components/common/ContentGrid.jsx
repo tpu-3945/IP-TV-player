c2dc7681b57e4ec19d9a3b23f1b9ebac4a8b32f0
Tueur_de_l€ak <vdsisaac410@gmail.com>
2025-10-29 02:45:42 +0100

    Update ContentGrid.jsx

--- src/components/common/ContentGrid.jsx
+++ src/components/common/ContentGrid.jsx
@@ -8,14 +8,4 @@ const ContentGrid = ({ items }) => (
   </div>
 );
 
-export default ContentGrid;```
-
-#### **`src/components/common/EmptyState.jsx`**
-```jsx
-const EmptyState = ({ message }) => (
-  <div className="flex justify-center items-center h-full min-h-[50vh] text-center">
-    <p className="text-xl text-gray-500 dark:text-gray-400">{message}</p>
-  </div>
-);
-
-export default EmptyState;
+export default ContentGrid;
