7265ebf9f2225a7c102f7b50b4b65072307705fa
Tueur_de_l€ak <vdsisaac410@gmail.com>
2025-10-29 01:22:24 +0100

    Change base path for GitHub Pages
    Updated base path for GitHub Pages configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Remplacez 'NOM_DU_REPO' par le nom de votre dépôt GitHub
-const REPO_NAME = 'iptv-player-gh-pages'; 
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${REPO_NAME}/`, // Important pour GitHub Pages
+  // Le chemin de base doit correspondre au nom de votre dépôt GitHub
+  base: '/IP-TV-player/',
 })
