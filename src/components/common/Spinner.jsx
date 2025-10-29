108e959096c952bac2e7f1e44b23564fd37aed29
Tueur_de_l€ak <vdsisaac410@gmail.com>
2025-10-29 02:40:46 +0100

    Refactor Spinner component for concise syntax

--- src/components/common/Spinner.jsx
+++ src/components/common/Spinner.jsx
@@ -1,10 +1,8 @@
-const Spinner = ({ message = 'Chargement...' }) => {
-  return (
-    <div className="flex flex-col justify-center items-center h-full min-h-[50vh] text-gray-500 dark:text-gray-400">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-indigo-500" />
-      <p className="mt-4 text-lg">{message}</p>
-    </div>
-  );
-};
+const Spinner = ({ message = 'Chargement...' }) => (
+  <div className="flex flex-col justify-center items-center h-full min-h-[50vh] text-gray-500 dark:text-gray-400">
+    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-indigo-500"></div>
+    <p className="mt-4 text-lg">{message}</p>
+  </div>
+);
 
 export default Spinner;
