18a6544e767d07c4f8bda11bc7d5b7e08aff9a66
Tueur_de_l€ak <vdsisaac410@gmail.com>
2025-10-29 01:17:49 +0100

    Configure Vite for GitHub Pages
    Set base URL for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// Remplacez 'NOM_DU_REPO' par le nom de votre dépôt GitHub
+const REPO_NAME = 'iptv-player-gh-pages'; 
+
+export default defineConfig({
+  plugins: [react()],
+  base: `/${REPO_NAME}/`, // Important pour GitHub Pages
+})
